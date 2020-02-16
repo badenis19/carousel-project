cb551b98384162784a9c6bc4cadb006be482f837
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-02-16 09:19:00 +0000

    card designed + added content + added buttons + buttons working (next and previous)

--- main.js
+++ main.js
@@ -1,87 +1,84 @@
 // TO DO
-// design top part
+// design top part - done
 // Design cards
 // button to go to next image
-// make the container overflow
+// make the container overflow - done
 
-const cards = [
+const CARDS = [
     {
         photo: "https://images.unsplash.com/photo-1565099824688-e93eb20fe622?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",
         title: "History",
-        description: "Louvre, blablablabla",
+        description: "Lorem ipsum dolor sit amet, consectetur adipis",
         price: "£102 per person",
         rating: 5
     },
     {
         photo: "https://images.unsplash.com/photo-1535043934128-cf0b28d52f95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
         title: "History",
-        description: "Louvre, blablablabla",
-        price: "£ per person",
+        description: "Lorem ipsum dolor sit amet, consectetur adipis",
+        price: "£34 per person",
         rating: 2
     },
     {
         photo: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",
         title: "Explore",
-        description: "Louvre, blablablabla",
+        description: "Lorem ipsum dolor sit amet, consectetur adipis",
         price: "£102 per person",
         rating: 4
     },
     {
         photo: "https://images.unsplash.com/photo-1535043934128-cf0b28d52f95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
         title: "Bike and Tase",
-        description: "Louvre, blablablabla",
-        price: "£102 per person",
+        description: "Lorem ipsum dolor sit amet, consectetur adipis",
+        price: "£10 per person",
         rating: 5
+    },
+    {
+        photo: "https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        title: "Discovery",
+        description: "Lorem ipsum dolor sit amet, consectetur adipis",
+        price: "£10 per person",
+        rating: 5
+    },
+    {
+        photo: "https://images.unsplash.com/photo-1535043934128-cf0b28d52f95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        title: "Sports",
+        description: "Lorem ipsum dolor sit amet, consectetur adipis",
+        price: "£102 per person",
+        rating: 1
     }
-    // {
-    //     photo: "https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    //     title: "Discovery",
-    //     description: "Louvre, blablablabla",
-    //     price: "£102 per person",
-    //     rating: 5
-    // },
-    // {
-    //     photo: "https://images.unsplash.com/photo-1535043934128-cf0b28d52f95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    //     title: "Sports",
-    //     description: "Louvre, blablablabla",
-    //     price: "£102 per person",
-    //     rating: 1
-    // }
 ]
 
 let cards_container = document.querySelector(".cards-container");
+let next = document.querySelector("#next");
+let previous = document.querySelector("#previous");
 
-if (cards_container){
-    // let button = document.querySelector("#add");
-    console.log("cards.length");
+if (cards_container) {
 
-    
-    // button.addEventListener('click', () => {
-
-        cards.forEach((card) => {
+    CARDS.forEach((card) => {
         cards_container.insertAdjacentHTML("beforeend",
-                `<div class="card">
+            `<div class="card">
                 <p> <img src=${card.photo} alt=""></p>
                 <p class="title">${card.title}</p>
                 <p class="descr">${card.description}</p>
                 <p class="price">${card.price}</p>
                 <p class="rating">${card.rating}</p>
-                </div>`)}
+                </div>`)
+    });
 
-        );
-    // })
+    if (next){
+        next.addEventListener('click', () => {
+            console.log(">>")
+            cards_container.scrollLeft += 233 ;
+        })
+    }
 
-    // button.addEventListener('click', () => {
-       
-    //     cards_container.insertAdjacentHTML("beforeend", 
-    //         `<div class="card">
-    //             <p>${cards[0].photo}</p>
-    //             <p>${cards[0].title}</p>
-    //             <p>${cards[0].description}</p>
-    //             <p>${cards[0].price}</p>
-    //         </div>`);
-    // })
-    
+    if (previous){
+        previous.addEventListener('click', () => {
+            console.log("<<")
+            cards_container.scrollLeft -= 233 ;
+        })
+    }
 }
 
 
